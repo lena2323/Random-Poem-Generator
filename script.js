8b705de9efa67493ff46886c37a8d065696268e5
Lena2323 <lena.jovicic23@gmail.com>
2022-05-03 09:11:36 +0200

    Fixed the stanzas! Good night's sleep ftw!

--- script.js
+++ script.js
@@ -12,65 +12,15 @@ generateRandomPoem.addEventListener('click',() =>{
 
 
 const poems = [
-    "For a change,\
-    I find myself in a character that took his own life;\
-    The last words he ever spoke \
-    Mirror the thoughts of my own\
-    And a friend told me once\
-    That I scare him when I talk at night \
-    About my what ifs \
-    About my fight to survive this pathetic form of life\
-    Sometimes I scare myself too\
-    But those feelings now\
-    Just come naturally"
+    "For a change, \n \n I find myself in a character that took his own life; \n The last words he ever spoke \n Mirror the thoughts of my own \n And a friend told me once \n That I scare him when I talk at night \n About my what ifs \n About my fight to survive this pathetic form of life \n Sometimes I scare myself too \n But those feelings now \n Just come naturally"
 
     ,
 
 
-    "Home\
-    I ran away from home 'cause\
-    Home used to be a home a long time ago..\
-    Home is being burnt down\
-    while you and I are just standing around,\
-    not moving, just watching,\
-    - not even breathing - \
-    simply waiting for it to stop.\
-    Home used to be a home\
-    a long time ago,\
-    'cause the old pipes don't offer comfort anymore\
-    and the door that creaks\
-    is starting to get on my nerves.\
-    Home used to be a home\
-    a long time ago,\
-    all the paint in the world\
-    can't cover the bloody walls\
-    in the old living room.\
-    Home used to be a home\
-    a long time ago,\
-    Now it's falling apart\
-    in front of our very eyes,\
-    and you can't save the walls\
-    that are slowly tumbling down.\
-    I ran away from home 'cause\
-    home stopped being a home a long time ago,\
-    you see, home has a heartbeat\
-    and it slowly fades away..\
-    And I will not fight\
-    for something that cannot be saved.\
-    ",
+    "Home\n \n I ran away from home 'cause\n Home used to be a home a long time ago..\n Home is being burnt down\n while you and I are just standing around,\n not moving, just watching,\n - not even breathing - \n simply waiting for it to stop.\n Home used to be a home\n a long time ago,\n 'cause the old pipes don't offer comfort anymore\n and the door that creaks\n is starting to get on my nerves.\n Home used to be a home\n a long time ago,\n all the paint in the world\n can't cover the bloody walls\n in the old living room.\n Home used to be a home\n a long time ago,\n Now it's falling apart\n in front of our very eyes,\n and you can't save the walls\n that are slowly tumbling down.\n I ran away from home 'cause\n home stopped being a home a long time ago,\n you see, home has a heartbeat\n and it slowly fades away..\n And I will not fight\n for something that cannot be saved.\n",
 
-    "Just\
-    Diamond - just a rock\
-    Music - just a chord\
-    Whimper - just a sound \
-    Scar - just a mark \
-    Violet - just a flower \
-    Courage - just stupidity\
-    Equality - just a term \
-    Love - just a feeling \
-    Life - just before death. \
-    Just.\
-    "
+    " Just\n \n Diamond - just a rock, \n  Music - just a chord \n Whimper - just a sound \n Scar - just a mark \n Violet - just a flower \n Courage - just stupidity\n Equality - just a term \n Love - just a feeling \n Life - just before death. \n Just."
+
 ]
 
 
